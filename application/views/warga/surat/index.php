d5932de4fac8e8286481db04067a850088e2ba31
satriyaadhip <satriya.pradana1702.sp@gmail.com>
2024-08-24 20:05:25 +0700

    revisi pukul 20.00

--- application/views/warga/surat/index.php
+++ application/views/warga/surat/index.php
@@ -1,76 +1,120 @@
 <style>
-    .img-icon {
-        max-width: 6%;
-    }
+	.img-icon {
+		max-width: 20%;
+	}
 
-    @media screen and (max-width:346px) {
-        .img-icon {
-            max-width: 3%;
-        }
-    }
+	@media screen and (max-width: 200px) {
+		.img-icon {
+			max-width: 10%;
+		}
+	}
 </style>
 <div class="main-content container-fluid">
-    <div class="page-title">
-        <h5><strong>List Surat</strong></h5>
-    </div>
-    <section class="section">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="row m-3">
-                    <div class="col-lg-12 overflow-auto">
-
-                        <!-- <a href="<?= base_url('sktm/buat-surat') ?>" class="mb-3">
-                            <div class="unread custom-alert-3 bg-white" role="alert">
-                                <div class="alert-text w-75 mt-2 p-4 d-flex flex-row">
-                                    <img src="<?= base_url() ?>./assets/icons/envelope.png" class="img-icon">
-                                    <h6 class=" font-bold ml-3 my-auto">Surat Keterangan Tidak Mampu</h6>
-                                </div>
-                            </div>
-                        </a> -->
-                        <!-- <a href="<?= base_url('sku/buat-surat') ?>" class="mb-3">
-                            <div class="unread custom-alert-3 bg-white" role="alert">
-                                <div class="alert-text w-75 mt-2 p-4 d-flex flex-row">
-                                    <img src="<?= base_url() ?>./assets/icons/envelope.png" class="img-icon">
-                                    <h6 class="font-bold ml-3 my-auto">Surat Keterangan Usaha</h6>
-                                </div>
-                            </div>
-                        </a> -->
-                        <a href="<?= base_url('skd/buat-surat') ?>" class="mb-3">
-                            <div class="unread custom-alert-3 bg-white" role="alert">
-                                <div class="alert-text w-75 mt-2 p-4 d-flex flex-row">
-                                    <img src="<?= base_url() ?>./assets/icons/envelope.png" class="img-icon">
-                                    <h6 class="font-bold ml-3 my-auto">Surat Keterangan Pindah</h6>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?= base_url('skp/buat-surat') ?>" class="mb-3">
-                            <div class="unread custom-alert-3 bg-white" role="alert">
-                                <div class="alert-text w-75 mt-2 p-4 d-flex flex-row">
-                                    <img src="<?= base_url() ?>./assets/icons/envelope.png" class="img-icon">
-                                    <h6 class="font-bold ml-3 my-auto">Surat Keterangan Pengantar</h6>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?= base_url('spak/buat-surat') ?>" class="mb-3">
-                            <div class="unread custom-alert-3 bg-white" role="alert">
-                                <div class="alert-text w-75 mt-2 p-4 d-flex flex-row">
-                                    <img src="<?= base_url() ?>./assets/icons/envelope.png" class="img-icon">
-                                    <h6 class="font-bold ml-3 my-auto">Surat Keterangan Akte Kelahiran</h6>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?= base_url('skk/buat-surat') ?>" class="mb-3">
-                            <div class="unread custom-alert-3 bg-white" role="alert">
-                                <div class="alert-text w-75 mt-2 p-4 d-flex flex-row">
-                                    <img src="<?= base_url() ?>./assets/icons/envelope.png" class="img-icon">
-                                    <h6 class="font-bold ml-3 my-auto">Surat Keterangan Kematian</h6>
-                                </div>
-                            </div>
-                        </a>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+	<div class="page-title">
+		<h5>
+			<strong>Pilih salah satu layanan surat berikut dibawah ini.</strong>
+		</h5>
+	</div>
+	<section class="section">
+		<div class="col-9">
+			<div class="row d-flex">
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<a href="<?= base_url('skd/buat-surat') ?>">
+						<div
+							class="mb-2"
+							style="
+								border-radius: 20px;
+								background-image: linear-gradient(
+									to bottom right,
+									#ef8c50,
+									#ef6445
+								);
+							"
+							role="alert"
+						>
+							<div class="alert-text mt-2 px-3 py-2">
+								<img
+									src="<?= base_url() ?>./assets/icons/logo_pengantar.png"
+									style="max-width: 64px; margin-top: 100px"
+								/>
+								<h6 class="text-white">Surat Keterangan<br />Pindah</h6>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<a href="<?= base_url('skp/buat-surat') ?>">
+						<div
+							class="mb-2"
+							style="
+								border-radius: 20px;
+								background-image: linear-gradient(
+									to bottom right,
+									#50d2ef,
+									#45b2ef
+								);
+							"
+							role="alert"
+						>
+							<div class="alert-text mt-2 px-3 py-2">
+								<img
+									src="<?= base_url() ?>./assets/icons/logo_pengantar.png"
+									style="max-width: 64px; margin-top: 100px"
+								/>
+								<h6 class="text-white">Surat Keterangan<br />Pengantar</h6>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<a href="<?= base_url('spak/buat-surat') ?>">
+						<div
+							class="mb-2"
+							style="
+								border-radius: 20px;
+								background-image: linear-gradient(
+									to bottom right,
+									#40dec2,
+									#00a6b4
+								);
+							"
+							role="alert"
+						>
+							<div class="alert-text mt-2 px-3 py-2">
+								<img
+									src="<?= base_url() ?>./assets/icons/logo_pengantar.png"
+									style="max-width: 64px; margin-top: 100px"
+								/>
+								<h6 class="text-white">Surat Keterangan<br />Akta Kelahiran</h6>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<a href="<?= base_url('skk/buat-surat') ?>">
+						<div
+							class="mb-2"
+							style="
+								border-radius: 20px;
+								background-image: linear-gradient(
+									to bottom right,
+									#59d8c1,
+									#2d8e7c
+								);
+							"
+							role="alert"
+						>
+							<div class="alert-text mt-2 px-3 py-2">
+								<img
+									src="<?= base_url() ?>./assets/icons/logo_pengantar.png"
+									style="max-width: 64px; margin-top: 100px"
+								/>
+								<h6 class="text-white">Surat Keterangan<br />Kematian</h6>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
